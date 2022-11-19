55b89e69d1d12fbb5eb9b95806aad311a9c80d06
mjh9401 <mjh9401@naver.com>
2022-11-19 23:54:49 +0900

    Model, Enum, Controller, Service 생성실습

--- nest-js/src/boards/boards.controller.ts
+++ nest-js/src/boards/boards.controller.ts
@@ -1,8 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post} from '@nestjs/common';
+import { brotliDecompressSync } from 'zlib';
+import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardService : BoardsService){}
 
+    @Get('/')
+    getAllBoard():Board[]{
+        return this.boardService.getAllBoards();
+    }
+    
+    @Post()
+    createBoard(
+        @Body('title') title:String,
+        @Body('description') description:String
+    ):Board{
+        return this.boardService.createBoard(title,description);
+    }
 }
--- nest-js/src/boards/boards.service.ts
+++ nest-js/src/boards/boards.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import {v1 as uuid} from 'uuid';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private board: Board[]  = [];
+
+    getAllBoards():Board[]{
+        return this.board;
+    }
+
+    createBoard(title:String, description:String){
+        const board:Board ={
+            id:uuid(),
+            title,
+            description,
+            status : BoardStatus.PUBLIC
+        } 
+
+        this.board.push(board);
+
+        return board;
+    }
+
+}
